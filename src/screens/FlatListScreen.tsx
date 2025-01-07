218328c53c46ee7ae4358b030078f341e41378a1
DGreegman <obeagug@gmail.com>
2025-01-08 00:58:58 +0100

    Added the dark/light theme to few other screens

--- src/screens/FlatListScreen.tsx
+++ src/screens/FlatListScreen.tsx
@@ -1,32 +1,44 @@
-import { Button, FlatList, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Button, FlatList, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {useTheme} from '../context/ThemeContext';
 
 const FLAT_DATA = Array.from({length: 100}, (_, i) => ({
-    key: i.toString(), 
-    title: `Item ${i + 1}`
-}))
-const FlatListScreen:React.FC = () => {
-    const handleRenderItem = ({item}:{item:{key:string, title:string}}) => {
-        return (
-        <View style={{padding:10}}>
-            <Text>{item.title}</Text>
-            {/* <Button title='Go to StackScreen2' onPress={()=>{}}/> */}
-        </View>
-        )
-    }
-    
+  key: i.toString(),
+  title: `Item ${i + 1}`,
+}));
+const FlatListScreen: React.FC = () => {
+  const {theme} = useTheme();
+  const isDarkMode = theme === 'dark';
+  const handleRenderItem = ({item}: {item: {key: string; title: string}}) => {
+    return (
+      <View
+        style={[
+          {padding: 10, backgroundColor: isDarkMode ? '#000000' : '#ffffff'},
+        ]}>
+        <Text
+          style={{
+            color: isDarkMode ? '#ffffff' : '#000000',
+            paddingBottom: 10,
+          }}>
+          {item.title}
+        </Text>
+        {/* <Button title='Go to StackScreen2' onPress={()=>{}}/> */}
+      </View>
+    );
+  };
+
   return (
     <View>
       <Text>FlatListScreen</Text>
-      <FlatList 
+      <FlatList
         data={FLAT_DATA}
         renderItem={handleRenderItem}
         keyExtractor={item => item.key}
-        />
+      />
     </View>
-  )
-}
+  );
+};
 
-export default FlatListScreen
+export default FlatListScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/screens/ModalScreen.tsx
+++ src/screens/ModalScreen.tsx
@@ -1,55 +1,79 @@
-import { Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
+import {Modal, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import {useTheme} from '../context/ThemeContext';
 
 const ModalScreen = () => {
-    const [modalVisible, setModalVisible] = useState(false)
+  const [modalVisible, setModalVisible] = useState(false);
+  const {theme} = useTheme();
+  const isDarkMode = theme === 'dark';
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: isDarkMode ? '#000000' : '#ffffff',
+        },
+      ]}>
       <Text style={styles.header}>ModalScreen</Text>
-      <TouchableOpacity style={styles.button} onPress={()=>setModalVisible(!modalVisible)}>
-            <Text style={styles.text}>Close Modal</Text>
-        </TouchableOpacity> 
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setModalVisible(!modalVisible)}>
+        <Text style={styles.text}>Open Modal</Text>
+      </TouchableOpacity>
 
-        <Modal
-            animationType='slide'
-            transparent={true}
-            visible={modalVisible}
-            onRequestClose={()=>setModalVisible(false)}
-        >
-            <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'rgba(0,0,0,0.5)'}}>
-                <View style={{backgroundColor:'white', padding:10, borderRadius:10}}>
-                    <Text style={styles.text}>This is a modal</Text>
-                    <TouchableOpacity style={styles.button} onPress={()=>setModalVisible(false)}>
-                        <Text style={styles.text}>Close Modal</Text>
-                    </TouchableOpacity> 
-                </View>
-            </View>
-        </Modal>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0,0,0,0.5)',
+          }}>
+          <View
+            style={{
+              backgroundColor: 'white',
+              padding: 10,
+              borderRadius: 10,
+              width: '100%',
+            }}>
+            <Text style={styles.text}>This is a modal</Text>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => setModalVisible(false)}>
+              <Text style={styles.text}>Close Modal</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
-  )
-}
+  );
+};
 
-export default ModalScreen
+export default ModalScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    header:{
-        fontWeight: 'bold',
-        fontSize: 25
-    },
-    text: {
-        padding: 10,
-        fontSize: 20,
-        color: 'white',
-        fontWeight: 'bold'  
-    },
-    button:{
-        padding: 10,
-        backgroundColor: 'blue',
-        borderRadius: 5
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 25,
+  },
+  text: {
+    padding: 10,
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  button: {
+    padding: 10,
+    backgroundColor: 'blue',
+    borderRadius: 5,
+  },
+});
